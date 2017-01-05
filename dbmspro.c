bfdf7225383f488cbae505c8b096b154551e356a
Shreyansh Gupta <shrey.20may@gmail.com>
2017-01-05 23:56:41 +0530

    Update dbmspro.c

--- dbmspro.c
+++ dbmspro.c
@@ -31,10 +31,11 @@ main()
 		fscanf(fp,"%s    %s    %d    %d    %d    %s    %s    %s    %s",f[i].batch_no,f[i].product_name,&f[i].price,&f[i].quantity_per_carton,&f[i].carton_in_stock,f[i].manufacturing_date,f[i].expiry_date,f[i].manufacturer,f[i].schedule);
 	}
 
-	//printf("%s      %s      %d      %d      %d      %s      %s      %s	%s\n",f[1].batch_no,f[1].product_name,f[1].price,f[1].quantity_per_carton,f[1].carton_in_stock,f[1].manufacturing_date,f[1].expiry_date,f[1].manufacturer,f[1].schedule);
 
-	
-	printf("Tell me Schedule H drugs in stock\n");
+	printf("\n");
+	printf("TELL ME SCHEDULE 'H' DRUG\n");
+	printf("__________________________\n");
+	printf("Batch_No                       Product_Name                     Price   QiC     CiS     Mfg_date        Exp._Date           Manufacturer\n");	
 	for (i=0; i<14; i++)
 	{	
 		if(strcmp(f[i].schedule,"H") == 0)
@@ -43,4 +44,3 @@ main()
 		}
 	}
 }
-
